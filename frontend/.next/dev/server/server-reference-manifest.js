5153f80e0f2670797719691037ddaaa69c75924a
Dharm Patel <dharmpatel0002@gmail.com>
2026-02-21 17:51:12 -0500

    Remove Next.js build artifacts and ignore .next

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FVCFT1/deQl5scpIawfIX9+7QDxrpBLyzJrX88JwpgU=\"\n}"
--- frontend/.next/dev/static/chunks/Desktop_hackhers_Deathbox_frontend_app_layout_tsx_4912b12e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__02806c9d._.css",
-  "static/chunks/4dee3_d6fe7ac0._.js"
-],
-    source: "dynamic"
-});
